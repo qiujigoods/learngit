a2af4480b6bfe0e0ee12bb2bf780611f4ce96cd7
wsf <2230379332@qq.com>
2019-06-24 17:31:37 +0800

    wsf

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -19,6 +19,14 @@ class RegisterController extends BaseController
 
 			$data['create_time'] = time();
 
+			if (DB::table('admin')->where('phone', $data['phone'])->first()) {
+				echo "该手机号已注册";die;
+			}
+
+			if (DB::table('admin')->where('email', $data['email'])->first()) {
+				echo "该邮箱已注册";die;
+			}
+
 			if (DB::table('admin')->insert($data)) {
 				return redirect('/admin');
 			}
